ced85a8569ea5f261d6ee5b1c972f5ca835744b8
Vitalii Kondratenko <v.kondratenko2002@gmail.com>
2024-03-26 00:14:00 +0200

    add multiple flies upload

--- client/config/url.config.ts
+++ client/config/url.config.ts
@@ -2,4 +2,4 @@ export const getUserUrl = (str: string) => `/user${str}`
 
 export const getAuthUrl = (str: string) => `/auth${str}`
 
-export const uploadFilesUrl = (folder?: string) => `/upload/?folder=${folder}`
+export const getFileUrl = (folder?: string) => `/upload/?folder=${folder}`
--- client/services/file.service.ts
+++ client/services/file.service.ts
@@ -1,12 +1,17 @@
-import {uploadFilesUrl} from "@/config/url.config";
+import { getFileUrl } from "@/config/url.config";
 import {AxiosInstance} from "axios";
 
 export const FileService = {
-    async uploadFile(apiAuth: AxiosInstance,file: FormData, folder?: string) {
-        return await apiAuth.post<{url: string, name: string}[]>(uploadFilesUrl(folder), file, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
+    async upload(apiAuth: AxiosInstance, files: FileList, folder?: string) {
+        const formData = new FormData()
+        for (let i = 0; i < files.length; i++) {
+            formData.append(`file`, files[i])
+        }
+
+        const { data } = await apiAuth.post<{
+            url: string,
+            name: string
+        }[]>(getFileUrl(folder ?? ''), formData, { headers: { "Content-Type": "multipart/form-data" } })
+        return data
     }
 }
