338e06138a48f47a1b5ae5fc9e869cf2ec147450
ZXC <topor4ik02@gmail.com>
2024-11-23 06:59:47 +0200

    fix error

--- client/components/Cell/index.tsx
+++ client/components/Cell/index.tsx
@@ -18,27 +18,27 @@ const Cell = async ({ style, params: { locale } }: Props) => {
       <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-28 max-md:gap-16 max-xl:gap-16 mt-6 text-4xl max-sm:text-3xl text-center gap-y-12 w-full items-end">
         <div className="flex flex-col gap-12 max-xl:gap-6 mt-6 w-full justify-end">
           <span>Наукова робота ОНТУ</span>
-          {researchWorks.data.map((researchWork, index) => (
-            <CellItem
-              key={researchWork.id}
-              text={researchWork.title}
-              image={index % 2 === 0 ? "/image/helmet.png" : "/image/atom.png"}
-              // link={`research/${researchWork.id}`}
-              link={`research`}
-            />
-          ))}
+          {/*{researchWorks.data.map((researchWork, index) => (*/}
+          {/*  <CellItem*/}
+          {/*    key={researchWork.id}*/}
+          {/*    text={researchWork.title}*/}
+          {/*    image={index % 2 === 0 ? "/image/helmet.png" : "/image/atom.png"}*/}
+          {/*    // link={`research/${researchWork.id}`}*/}
+          {/*    link={`research`}*/}
+          {/*  />*/}
+          {/*))}*/}
         </div>
         <div className="flex flex-col gap-12 max-xl:gap-6 mt-6 w-full justify-end">
           <span>Міжнародна діяльність ОНТУ</span>
-          {activities.data.map((item, index) => (
-            <CellItem
-              key={item.id}
-              text={item.title}
-              image={index % 2 !== 0 ? "/image/helmet.png" : "/image/atom.png"}
-              link={`international`}
-              // link={`international/${item.id}`}
-            />
-          ))}
+          {/*{activities.data.map((item, index) => (*/}
+          {/*  <CellItem*/}
+          {/*    key={item.id}*/}
+          {/*    text={item.title}*/}
+          {/*    image={index % 2 !== 0 ? "/image/helmet.png" : "/image/atom.png"}*/}
+          {/*    link={`international`}*/}
+          {/*    // link={`international/${item.id}`}*/}
+          {/*  />*/}
+          {/*))}*/}
         </div>
       </div>
     </div>
