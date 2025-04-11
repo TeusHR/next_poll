1fb4bce04401ddcaa2649d0a981cd4825fa47d75
sennqq <topor4ik02@gmail.com>
2025-04-11 23:34:32 +0300

    fix sitemap

--- client/app/[locale]/(client)/events/sitemap.ts
+++ client/app/[locale]/(client)/events/sitemap.ts
@@ -8,7 +8,7 @@ type Props = {
 };
 
 export default async function sitemap({ params: { locale } }: Props): Promise<MetadataRoute.Sitemap> {
-  const data = await EventsService.getAll(locale.toUpperCase());
+  const data = await EventsService.getAll("date", "asc", locale.toUpperCase());
   return data.map((item) => ({
     url: `${process.env.NEXTAUTH_URL}/uk/events/${item.id}`,
     lastModified: new Date(),
