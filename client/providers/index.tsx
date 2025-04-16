040388c9eaab0e796b6765027e666e2e7e82c3c5
sennqq <topor4ik02@gmail.com>
2025-04-16 03:27:51 +0300

    fix auth provider

--- client/providers/index.tsx
+++ client/providers/index.tsx
@@ -1,17 +1,14 @@
 "use client";
 import {SessionProvider} from "next-auth/react";
 import {ToastContainer} from "react-toastify";
-import AuthProvider from "./auth.provider";
 import {PropsWithChildren} from "react";
 import {HeroUIProvider} from "@heroui/react";
 
 const Providers = ({children}: PropsWithChildren) => {
         return <SessionProvider>
         <HeroUIProvider>
-            <AuthProvider>
                 <ToastContainer limit={3} autoClose={5000} closeOnClick newestOnTop position="bottom-center" pauseOnHover/>
                 {children}
-            </AuthProvider>
         </HeroUIProvider>
     </SessionProvider>
 }
